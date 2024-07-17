42db18c1632146787bd8515fe58568032a5a7ad0
Kiril <ozergart@gmail.com>
2024-07-17 19:07:10 +0300

    HW4

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Request, Response } from "express";
 
 import { ApiError } from "./errors/api-error";
 import { userRouter } from "./routers/user.router";
@@ -12,21 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/users", userRouter);
 app.use("/users/:userId", userRouter);
 app.use("/users", userRouter);
-app.put("/users/:userId", userRouter);
-
 app.use("/users/:userId", userRouter);
-app.use(
-  "*",
-  (err: ApiError, req: Request, res: Response, next: NextFunction) => {
-    res.status(err.status || 500).json(err.message);
-  },
-);
+app.use("/users/:userId", userRouter);
+app.use("*", (err: ApiError, req: Request, res: Response) => {
+  res.status(err.status || 500).json(err.message);
+});
 process.on("uncaughtException", (e) => {
   console.error("uncaughtException", e.message, e.stack);
   process.exit(1);
 });
 fsService.usersFromFile().then(() => {
   app.listen(3000, () => {
-    console.log("server is");
+    console.log("server is running");
   });
 });
