f5b4f30ccdc6fa847acc7db01b25cc92c6f02a52
Kiril <ozergart@gmail.com>
2024-07-17 20:50:31 +0300

    HW4

--- src/reposetories/user.reposetory.ts
+++ src/reposetories/user.reposetory.ts
@@ -1,6 +1,8 @@
 import { ApiError } from "../errors/api-error";
 import { IUser } from "../interfaces/user.interface";
 import { fsService } from "../services/fsService";
+import { emailValidator } from "../validators/emailValidator";
+import { passwordValidator } from "../validators/passwordValidator";
 
 class UserRepository {
   public async getList(): Promise<IUser[]> {
@@ -53,22 +55,26 @@ class UserRepository {
   ): Promise<IUser> {
     const users = await fsService.usersFromFile();
     const user = users.find((user) => user.id === userID);
-    const index = users.findIndex((user) => user.email === email);
-    if (index !== -1) {
-      throw new ApiError("user with this email already exists", 409);
-    } else {
-      if (email) {
-        user.email = email;
-      }
-      if (password) {
-        user.password = password;
-      }
-      if (name) {
-        user.name = name;
-      }
-      await fsService.usersToFile(users);
-      return user;
+    if (!user) {
+      throw new ApiError("User not found", 404);
     }
+    if (email && !emailValidator(email)) {
+      throw new ApiError("Invalid email format", 400);
+    }
+    if (password && !passwordValidator(password)) {
+      throw new ApiError("Invalid password format", 400);
+    }
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+    if (name) {
+      user.name = name;
+    }
+    await fsService.usersToFile(users);
+    return user;
   }
 }
 export const userRepository = new UserRepository();
